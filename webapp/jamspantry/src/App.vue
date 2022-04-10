6a176d49f075596bf2e9fcc155a48584ae4b4193
matt-hall0100 <matt.hall0100@gmail.com>
2022-04-09 22:29:57 -0500

    Created SignIn/SignUp Page with authentication

--- webapp/jamspantry/src/App.vue
+++ webapp/jamspantry/src/App.vue
@@ -2,13 +2,15 @@
   <v-app id="inspire">
     <v-app-bar app class="secondary" flat>
       <v-container class="py-0 fill-height">
-        <v-avatar size="54px">
-          <img src="./assets/JAMSLogo.png" />
+        <v-avatar 
+        size="54px"
+        @click="$router.push('/dashboard')"
+        ><img src="./assets/JAMSLogo.png" />
         </v-avatar>
-
-        <v-btn v-for="link in links" :key="link" text>
-          {{ link }}
-        </v-btn>
+        <v-toolbar-title
+        style="cursor: pointer"
+        @click="$router.push('/dashboard')"
+        ><h2>JAMSPantry</h2></v-toolbar-title>
 
         <v-spacer></v-spacer>
 
@@ -32,16 +34,27 @@
       </v-container>
     </v-app-bar>
 
-    <v-main class="primary">
-      <v-container>
-        
-      </v-container>
-    </v-main>
+    <v-main class="ma-4"><router-view /></v-main>
+    <toastItem ref="toastItem" />
+
   </v-app>
 </template>
 
 <script>
+import toastItem from "./components/Toast-Item.vue"
+import { signOut, getAuth } from "firebase/auth"
 export default {
+  components: {
+    toastItem,
+  },
+  mounted() {
+    this.$root.toastItem = this.$refs.toastItem;
+  },
+  computed: {
+    isSignedIn() {
+      return this.$route.name !== "Login" && this.$route.name !== "Poll";
+    },
+  },
   data: () => ({
     links: ["Dashboard", "Inventory"],
     searchItems: ["Apple", "Banana", "Cereal", "Rice"], /// need to query db for this list
@@ -49,6 +62,17 @@ export default {
   methods: {
     toggleTheme() {
       this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
+    },
+    logout() {
+      const auth = getAuth();
+      signOut(auth)
+        .then(() => {
+          this.$router.push("/");
+        })
+        .catch((error) => {
+          alert(error.message);
+          this.$router.push("/");
+        });
     },
   },
 };
