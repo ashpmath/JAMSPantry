c0e5be23f8108b055889852e00424caddd94d270
matt-hall0100 <matt.hall0100@gmail.com>
2022-04-15 15:02:24 -0500

    Updated Accent Color

--- webapp/jamspantry/src/plugins/vuetify.js
+++ webapp/jamspantry/src/plugins/vuetify.js
@@ -9,12 +9,12 @@ export default new Vuetify({
             light: {
                 primary: '#fcfdf6',
                 secondary: '#dee8cf',
-                accent: '#c9ecef',
+                accent: '#dd636e',
             },
             dark: {
                 primary: '#1b1c19',
                 secondary: '#a5b198',
-                accent: '#c1eef0',
+                accent: '#dd636e',
             },
         },
         dark: true, 
