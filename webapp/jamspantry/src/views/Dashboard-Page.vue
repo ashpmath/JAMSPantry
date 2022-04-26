df02903498a12fd6e7f2772fa65a0f373a9d2608
Ashley P. Mathews <ashpmath@gmail.com>
2022-04-25 23:02:46 -0500

    Added dashboard and tabs that user can select with search bar to the file dashboard-page.vue.

--- webapp/jamspantry/src/views/Dashboard-Page.vue
+++ webapp/jamspantry/src/views/Dashboard-Page.vue
@@ -1,7 +1,56 @@
 <template>
     <div>
-        <strong>DASHBOARD</strong>
+        <v-card>
+            <v-toolbar
+            color="gray"
+            dark
+            flat
+            >
+            <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+            <v-toolbar-title>Your Dashboard</v-toolbar-title>
+
+            <v-spacer></v-spacer>
+
+            <v-btn icon>
+                <v-icon>mdi-magnify</v-icon>
+            </v-btn>
+
+            <v-btn icon>
+                <v-icon>mdi-dots-vertical</v-icon>
+            </v-btn>
+
+            <template v-slot:extension>
+                <v-tabs
+                v-model="tab"
+                align-with-title
+                >
+                <v-tabs-slider color="yellow"></v-tabs-slider>
+
+                <v-tab
+                    v-for="item in items"
+                    :key="item"
+                >
+                    {{ item }}
+                </v-tab>
+                </v-tabs>
+            </template>
+            </v-toolbar>
+
+            <v-tabs-items v-model="tab">
+            <v-tab-item
+                v-for="item in items"
+                :key="item"
+            >
+                <v-card flat>
+                <v-card-text v-text="text"></v-card-text>
+                </v-card>
+            </v-tab-item>
+            </v-tabs-items>
+        </v-card>
+
         <v-container fluid>
+
         <v-spacer></v-spacer>
         Notifications & Alerts
 
@@ -39,7 +88,7 @@
             <th>Quantity</th>
             <th>Expiration Date</th>
         </thead>
-    <tbody>
+     <tbody>
         <tr>
             <td>Rice</td>
             <td>1</td>
@@ -67,8 +116,20 @@
         <canvas id="myChart"></canvas>
     </div>
 
-
         </v-container>
     </div>
 </template>
 
+<script>
+  export default {
+    data () {
+      return {
+        tab: null,
+        items: [
+          'Home', 'Kiosk', 'Inventory', 'Analytics',
+        ],
+        text: 'Welcome to JAMSPantry home page! Here you will find everything you need to know about your pantrys inventory to product information, expiration dates, and analytics.'
+      }
+    },
+  }
+</script>
