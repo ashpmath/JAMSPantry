7a620cdfc152e226bf3cb1450ecba80d3614a6d4
Sam Murphy <1777murphsam@gmail.com>
2022-04-28 16:26:31 -0500

    old dashboard moved here

--- webapp/jamspantry/src/views/Inventory-Page.vue
+++ webapp/jamspantry/src/views/Inventory-Page.vue
@@ -1,5 +1,85 @@
 <template>
-    <v-container fill-height fluid>
-        Inventory
+  <div>
+    <v-container>
+    <v-data-table
+        :headers="headers"
+        :items="desserts"
+        :items-per-page="5"
+        item-key="name"
+        class="elevation-1"
+        :footer-props="{
+            showFirstLastPage: true,
+            firstIcon: 'mdi-arrow-collapse-left',
+            lastIcon: 'mdi-arrow-collapse-right',
+            prevIcon: 'mdi-minus',
+            nextIcon: 'mdi-plus'
+    }"
+    
+    ></v-data-table>
+    
+    <div class="chart">
+        <canvas id="myChart"></canvas>
+    </div>
+    
     </v-container>
+  </div>
 </template>
+
+<script>
+  export default {
+    data () {
+      return {
+        headers: [
+          {
+            text: 'Pantry Good',
+            align: 'start',
+            value: 'name',
+          },
+          { text: 'Expiration Date', value: 'category' },
+        ],
+        desserts: [
+          {
+            name: 'Frozen Yogurt',
+            category: '4/22/23',
+          },
+          {
+            name: 'Ice cream sandwich',
+            category: '4/22/23',
+          },
+          {
+            name: 'Eclair',
+            category: '4/22/23',
+          },
+          {
+            name: 'Cupcake',
+            category: '4/22/23',
+          },
+          {
+            name: 'Gingerbread',
+            category: '4/22/23',
+          },
+          {
+            name: 'Jelly bean',
+            category: '4/22/23',
+          },
+          {
+            name: 'Lollipop',
+            category: '4/22/23',
+          },
+          {
+            name: 'Honeycomb',
+            category: '4/22/23',
+          },
+          {
+            name: 'Donut',
+            category: '4/22/23',
+          },
+          {
+            name: 'KitKat',
+            category: '4/22/23',
+          },
+        ],
+      }
+    },
+  }
+</script>
