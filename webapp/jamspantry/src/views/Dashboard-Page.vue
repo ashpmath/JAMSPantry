812b2185f46b6119002bd6cd3bb4109465100a96
matt-hall0100 <matt.hall0100@gmail.com>
2022-04-28 14:22:28 -0500

    Moved dashboard page to the inventory page and reverted dashboard.

--- webapp/jamspantry/src/views/Dashboard-Page.vue
+++ webapp/jamspantry/src/views/Dashboard-Page.vue
@@ -1,136 +1,121 @@
 <template>
-    <div>
-        <v-card>
-            <v-toolbar
-            color="gray"
-            dark
-            flat
-            >
-            <v-app-bar-nav-icon></v-app-bar-nav-icon>
+  <div>
+    <v-card>
+      <v-toolbar color="gray" dark flat>
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
-            <v-toolbar-title>Your Dashboard</v-toolbar-title>
-
-            <v-spacer></v-spacer>
-
-            <v-btn icon>
-                <v-icon>mdi-magnify</v-icon>
-            </v-btn>
-
-            <v-btn icon>
-                <v-icon>mdi-dots-vertical</v-icon>
-            </v-btn>
-
-            <template v-slot:extension>
-                <v-tabs
-                v-model="tab"
-                align-with-title
-                >
-                <v-tabs-slider color="yellow"></v-tabs-slider>
-
-                <v-tab
-                    v-for="item in items"
-                    :key="item"
-                >
-                    {{ item }}
-                </v-tab>
-                </v-tabs>
-            </template>
-            </v-toolbar>
-
-            <v-tabs-items v-model="tab">
-            <v-tab-item
-                v-for="item in items"
-                :key="item"
-            >
-                <v-card flat>
-                <v-card-text v-text="text"></v-card-text>
-                </v-card>
-            </v-tab-item>
-            </v-tabs-items>
-        </v-card>
-
-        <v-container fluid>
+        <v-toolbar-title>Your Dashboard</v-toolbar-title>
 
         <v-spacer></v-spacer>
-        Notifications & Alerts
-        <v-spacer></v-spacer>
-        
-        <v-alert
-            color="red"
-            dense
-            dismissible
-            elevation="20"
-            outlined
-            prominent
-            text-color="black"
-            text
-            type="error"
-            >
-            Food Expires Soon!
-        </v-alert>
-        <v-alert
-            dense
-            dismissible
-            elevation="20"
-            outlined
-            prominent
-            text
-            type="success"
-            >
-            The product <strong>rice</strong> was<strong> successfully added </strong>to pantry.
-        </v-alert>
 
-        <v-spacer></v-spacer>
-        Analytics
-        
-        <table class="table" id="dataTable">
+        <v-btn icon>
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+
+        <v-btn icon>
+          <v-icon>mdi-dots-vertical</v-icon>
+        </v-btn>
+
+        <template v-slot:extension>
+          <v-tabs v-model="tab" align-with-title>
+            <v-tabs-slider color="yellow"></v-tabs-slider>
+
+            <v-tab v-for="item in items" :key="item">
+              {{ item }}
+            </v-tab>
+          </v-tabs>
+        </template>
+      </v-toolbar>
+
+      <v-tabs-items v-model="tab">
+        <v-tab-item v-for="item in items" :key="item">
+          <v-card flat>
+            <v-card-text v-text="text"></v-card-text>
+          </v-card>
+        </v-tab-item>
+      </v-tabs-items>
+    </v-card>
+
+    <v-container fluid>
+      <v-spacer></v-spacer>
+      Notifications & Alerts
+      <v-spacer></v-spacer>
+
+      <v-alert
+        color="red"
+        dense
+        dismissible
+        elevation="20"
+        outlined
+        prominent
+        text-color="black"
+        text
+        type="error"
+      >
+        Food Expires Soon!
+      </v-alert>
+      <v-alert
+        dense
+        dismissible
+        elevation="20"
+        outlined
+        prominent
+        text
+        type="success"
+      >
+        The product <strong>rice</strong> was<strong>
+          successfully added </strong
+        >to pantry.
+      </v-alert>
+
+      <v-spacer></v-spacer>
+      Analytics
+
+      <table class="table" id="dataTable">
         <thead>
-            <th>Product</th>
-            <th>Quantity</th>
-            <th>Expiration Date</th>
+          <th>Product</th>
+          <th>Quantity</th>
+          <th>Expiration Date</th>
         </thead>
-     <tbody>
-        <tr>
+        <tbody>
+          <tr>
             <td>Rice</td>
             <td>1</td>
             <td>06/26/2022</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <td>Cereal</td>
             <td>3</td>
             <td>07/2/2023</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <td>Chicken Noodle Soup</td>
             <td>4</td>
-             <td>02/23/2023</td>
-        </tr>
-        <tr>
+            <td>02/23/2023</td>
+          </tr>
+          <tr>
             <td>Gold Fish</td>
             <td>2</td>
             <td>06/12/2022</td>
-        </tr>
-    </tbody>
-    </table>
- 
-    <div class="chart">
-        <canvas id="myChart"></canvas>
-    </div>
+          </tr>
+        </tbody>
+      </table>
 
-        </v-container>
-    </div>
+      <div class="chart">
+        <canvas id="myChart"></canvas>
+      </div>
+    </v-container>
+  </div>
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        tab: null,
-        items: [
-          'Home', 'Kiosk', 'Inventory', 'Analytics',
-        ],
-        text: 'Welcome to JAMSPantry home page! Here you will find everything you need to know about your pantrys inventory to product information, expiration dates, and analytics.'
-      }
-    },
-  }
+export default {
+  data() {
+    return {
+      tab: null,
+      items: ["Home", "Kiosk", "Inventory", "Analytics"],
+      text: "Welcome to JAMSPantry home page! Here you will find everything you need to know about your pantrys inventory to product information, expiration dates, and analytics.",
+    };
+  },
+};
 </script>
