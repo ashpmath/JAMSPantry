44a4012bdb98884b94d64e6e7fad6eb2e72197a0
Ashley P. Mathews <ashpmath@gmail.com>
2022-04-29 17:30:54 -0500

    editing dashboard title

--- webapp/jamspantry/src/views/Dashboard-Page.vue
+++ webapp/jamspantry/src/views/Dashboard-Page.vue
@@ -67,46 +67,10 @@
           successfully added </strong
         >to pantry.
       </v-alert>
-
       <v-spacer></v-spacer>
-      Analytics
-
-      <table class="table" id="dataTable">
-        <thead>
-          <th>Product</th>
-          <th>Quantity</th>
-          <th>Expiration Date</th>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Rice</td>
-            <td>1</td>
-            <td>06/26/2022</td>
-          </tr>
-          <tr>
-            <td>Cereal</td>
-            <td>3</td>
-            <td>07/2/2023</td>
-          </tr>
-          <tr>
-            <td>Chicken Noodle Soup</td>
-            <td>4</td>
-            <td>02/23/2023</td>
-          </tr>
-          <tr>
-            <td>Gold Fish</td>
-            <td>2</td>
-            <td>06/12/2022</td>
-          </tr>
-        </tbody>
-      </table>
-
-      <div class="chart">
-        <canvas id="myChart"></canvas>
-      </div>
 
       <div>
-        <h1>Analytics</h1>
+        <h1>Smart Pantry Environment</h1>
         <bar-chart></bar-chart>
       </div>
       <div id="app">
