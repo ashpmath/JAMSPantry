9115f87802ad16c81a05c9799e445125fc8d5873
Ashley P. Mathews <ashpmath@gmail.com>
2022-05-03 11:02:01 -0500

    edited analytics page

--- webapp/jamspantry/src/main.js
+++ webapp/jamspantry/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router'
+//import App from './graph.vue'
 
 Vue.config.productionTip = false
 
--- webapp/jamspantry/src/views/Analytics-Page.vue
+++ webapp/jamspantry/src/views/Analytics-Page.vue
@@ -1,52 +1,81 @@
 <template>
   <div>
     <v-card>
-      <v-toolbar color="gray" dark flat>
-        <v-app-bar-nav-icon></v-app-bar-nav-icon>
-        <v-toolbar-title>Your Dashboard</v-toolbar-title>
-        <v-spacer></v-spacer>
-        <v-btn icon>
-          <v-icon>mdi-magnify</v-icon>
-        </v-btn>
-        <v-btn icon>
-          <v-icon>mdi-dots-vertical</v-icon>
-        </v-btn>
-        <template v-slot:extension>
-          <v-tabs v-model="tab" align-with-title>
-            <v-tabs-slider color="yellow"></v-tabs-slider>
-            <v-tab v-for="item in items" :key="item">
-              {{ item }}
-            </v-tab>
-          </v-tabs>
-        </template>
-      </v-toolbar>
-      <v-tabs-items v-model="tab">
-        <v-tab-item v-for="item in items" :key="item">
-          <v-card flat>
-            <v-card-text v-text="text"></v-card-text>
-          </v-card>
-        </v-tab-item>
-      </v-tabs-items>
+      <div id="app" style="width:70%;">
+    <h1 style="padding-center:80px;">Pantry Temperature and Humidity</h1>
+    <GChart
+      type="AreaChart"
+      color="green"
+      :data="chartData"
+      :options="chartOptions"
+    />    
+  </div>
     </v-card>
     <v-container fluid>
-      <v-spacer></v-spacer>
-      <div>
-        <h1>Analytics</h1>
-        <line-chart></line-chart>
-      </div>
-      <div id="app">
-    <h2 style="padding-center:90px;">Expiration Dates</h2>
-    <GChart type="LineChart" :data="chartData" :options="chartOptions"/>    
-  </div>
+      
     </v-container>
   </div>
 </template>
 
+ <script>
+// //import { getDatabase } from '@firebase/database';
+// import { db, auth} from "../firebase";
+// import { ref, onValue, } from "firebase/database";
+// import { GChart } from "vue-google-charts";
+
+// export default {
+//   name: "App",
+//   components: {
+//     GChart
+//   },
+//   methods: {
+//     getTemperature(){
+//       onValue(ref(db, "/" + auth.currentUser.uid + "/Environment/temperature"), (snapshot) => {
+//         console.log(snapshot.val);
+//     });
+//     },
+//     getHumidity(){
+//       onValue(ref(db, "/" + auth.currentUser.uid + "/Environment/humidity"), (snapshot) => {
+//         console.log(snapshot.val);
+//     });
+//     },
+//   },
+//   data() {
+//     return {
+//       tab: null,
+//       items: ["Home", "Kiosk", "Inventory", "Analytics"],
+      
+//       created() {
+//       const firebase = firebase.firebase();
+//       },
+  
+//       // Array will be automatically processed with visualization.arrayToDataTable function
+//       chartData: [
+//         ["Product", "Expiration Date"],
+//         ["Temperature", this.getTemperature()], //getProductDate()
+//         ["Humidity", this.getHumidity()],
+//       ],
+//       chartOptions: {
+//         chart: {
+//           title: "Expiration Dates",       
+//           xAxis: "Item",
+//           yAxis: "Date Product Expires",
+//           legend: 'on'
+//         }
+//       }
+//     };
+//   }, // end of data
+//   mounted(){
+//     this.getTemperature();
+//     this.getHumidity();
+//   },
+// };
+// </script>
+
 <script>
-//import { getDatabase } from '@firebase/database';
+import { GChart } from "vue-google-charts";
 import { db, auth} from "../firebase";
 import { ref, onValue, } from "firebase/database";
-import { GChart } from "vue-google-charts";
 
 export default {
   name: "App",
@@ -67,33 +96,24 @@ export default {
   },
   data() {
     return {
-      tab: null,
-      items: ["Home", "Kiosk", "Inventory", "Analytics"],
-      
-      created() {
-      const firebase = firebase.firebase();
-      },
-  
       // Array will be automatically processed with visualization.arrayToDataTable function
       chartData: [
-        ["Product", "Expiration Date"],
-        ["Temperature", this.getTemperature()], //getProductDate()
-        ["Humidity", this.getHumidity()],
+        ["Product", "Environment"],
+        ["Temperature", 20], //this.getTemperature()
+        ["Humidity", 10],    //this.getHumidity()
       ],
       chartOptions: {
         chart: {
-          title: "Expiration Dates",       
-          xAxis: "Item",
-          yAxis: "Date Product Expires",
-          legend: 'on'
+          title: "Temperature and Humidity",
+          subtitle: "By Hour"
         }
-        
       }
     };
-  }, // end of data
+  },
   mounted(){
     this.getTemperature();
     this.getHumidity();
   },
 };
 </script>
+
