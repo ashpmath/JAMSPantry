47cbd1fe4f79a25ecd877b5e41b1018639cbe1ae
Sam Murphy <1777murphsam@gmail.com>
2022-05-04 19:21:15 -0500

    alerts for homepage

--- webapp/jamspantry/src/views/Home-Page.vue
+++ webapp/jamspantry/src/views/Home-Page.vue
@@ -9,7 +9,6 @@
             <v-alert
                 color="red"
                 dense
-                dismissible
                 elevation="20"
                 outlined
                 prominent
@@ -17,23 +16,128 @@
                 text
                 type="error"
             >
-                Food Expires Soon!
+            <v-data-table
+            hide-default-header
+            hide-default-footer
+            :headers="headers"
+            :items="expired"
+            :items-per-page="5"
+            item-key="name"
+            class="elevation-1">
+            </v-data-table>  
             </v-alert>
             <v-alert
+                color="yellow"
                 dense
-                dismissible
                 elevation="20"
                 outlined
                 prominent
+                text-color="black"
                 text
-                type="success"
+                type="error"
             >
-                The product <strong>rice</strong> was<strong>
-                successfully added </strong
-                >to pantry.
-            </v-alert>
-                        
+            <v-data-table
+            hide-default-header
+            hide-default-footer
+            :headers="headers"
+            :items="warning"
+            :items-per-page="5"
+            item-key="name"
+            class="elevation-1">
+            </v-data-table>  
+            </v-alert>        
         </v-container>
     </div>
 </template>
 
+<script>
+import { auth, db } from "../firebase";
+import { ref, query, get, orderByChild } from "firebase/database";
+
+export default {
+  data() {
+    return {
+      headers: [
+        {
+          text: "Pantry Good",
+          align: "start",
+          value: "description",
+        },
+        { text: "Expiration Date", value: "expir" },
+      ],
+      expired: [],
+      warning: [],
+    };
+  },
+  
+  methods: {
+    getAlerts() {
+      get(
+        query(
+          ref(db, auth.currentUser.uid + "/inventory"),
+          orderByChild("description")
+        )
+      ).then((snapshot) => {
+        if (snapshot.exists()) {
+            const year = new Date().getFullYear()
+            const month = new Date().getMonth()+1
+            const day = new Date().getDay()
+            console.log(snapshot.val());
+            // expiration for loop
+            for (const itemKey in snapshot.val()) {
+                let dateArray = snapshot.val()[itemKey]["expiration"].split("/");
+                // check if year is correct
+                if (dateArray[2]<year){
+                    this.expired.push({
+                                description: snapshot.val()[itemKey].description,
+                                expir: snapshot.val()[itemKey].expiration,
+                            })
+                }
+                else{ if (dateArray[2]==year & dateArray[0]<month){
+                    this.expired.push({
+                                description: snapshot.val()[itemKey].description,
+                                expir: snapshot.val()[itemKey].expiration,
+                            })
+                }
+                    else{ if(dateArray[0]==month & dateArray[1]<day){
+                        this.expired.push({
+                                description: snapshot.val()[itemKey].description,
+                                expir: snapshot.val()[itemKey].expiration,
+                            })
+                    }
+
+                    }
+                }    
+            }
+            const check = new Date()
+            check.setDate(check.getDate()+7)
+            const dayCheck = check.getDate()
+            const monthCheck = check.getMonth()+1
+            const yearCheck = check.getFullYear()
+            //warning loop
+            for (const itemKey in snapshot.val()) {
+                let dateArray = snapshot.val()[itemKey]["expiration"].split("/");
+                if (dateArray[2]<=yearCheck & dateArray[2]>=year){
+                    if (dateArray[0]<=monthCheck & dateArray[0]>=month){
+                        if (dateArray[1]<=dayCheck & dateArray[1]>=day){
+                                this.warning.push({
+                                    description: snapshot.val()[itemKey].description,
+                                    expir: snapshot.val()[itemKey].expiration,
+                                })
+                        }
+                    }
+                }
+                   
+            }
+        } else {
+          this.expired = [];
+        }
+      });
+    },
+    },
+
+  mounted() {
+    this.getAlerts();
+  },
+};
+</script>
