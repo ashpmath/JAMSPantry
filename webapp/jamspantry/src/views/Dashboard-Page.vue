467634dfd0ca3a9483976d73668d5c835e4f1ecc
matt-hall0100 <matt.hall0100@gmail.com>
2022-05-05 23:20:35 -0500

    added icons to tabs

--- webapp/jamspantry/src/views/Dashboard-Page.vue
+++ webapp/jamspantry/src/views/Dashboard-Page.vue
@@ -1,40 +1,38 @@
 <template>
-  <v-card>
-    <v-toolbar>
+  <v-card style="max-width: 1300px; margin: auto">
+    <v-toolbar mt-3>
       <template>
-        <v-tabs
-          v-model="tabs"
-          centered>
-          <v-tab>Home</v-tab>
-          <v-tab>Inventory</v-tab>
-          <v-tab>Analytics</v-tab>
+        <v-tabs v-model="tabs" centered icons-and-text>
+          <v-tab>Home<v-icon>mdi-home</v-icon></v-tab>
+          <v-tab>Inventory<v-icon>mdi-format-list-bulleted</v-icon></v-tab>
+          <v-tab>Analytics<v-icon>mdi-poll</v-icon></v-tab>
         </v-tabs>
       </template>
     </v-toolbar>
     <v-tabs-items v-model="tabs">
-        <v-tab-item>
-            <v-container>
-                <home></home>
-            </v-container>
-        </v-tab-item>
-        <v-tab-item>
-            <v-container>
-                <inventory></inventory>
-            </v-container>
-        </v-tab-item>
-        <v-tab-item>
-            <v-container>
-                <analytics></analytics>
-            </v-container>
-        </v-tab-item>
+      <v-tab-item>
+        <v-container>
+          <home></home>
+        </v-container>
+      </v-tab-item>
+      <v-tab-item>
+        <v-container>
+          <inventory></inventory>
+        </v-container>
+      </v-tab-item>
+      <v-tab-item>
+        <v-container>
+          <analytics></analytics>
+        </v-container>
+      </v-tab-item>
     </v-tabs-items>
   </v-card>
 </template>
 
 <script>
-import Inventory from './Inventory-Page.vue'
-import Analytics from './Analytics-Page.vue'
-import Home from './Home-Page.vue'
+import Inventory from "./Inventory-Page.vue";
+import Analytics from "./Analytics-Page.vue";
+import Home from "./Home-Page.vue";
 
 export default {
   name: "App",
@@ -44,10 +42,9 @@ export default {
     Home,
   },
   data() {
-    return {  
+    return {
       tabs: null,
-      created() {
-      },
+      created() {},
       // Array will be automatically processed with visualization.arrayToDataTable function
     };
   }, //end of data method
@@ -56,6 +53,6 @@ export default {
 
 <style>
 .v-tab--active {
-    color: #e00736 !important;
+  color: #e00736 !important;
 }
 </style>
